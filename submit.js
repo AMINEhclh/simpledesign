e8343ae0fd18a09535f1eac634ca4ce5048b5434
AMINEhclh <93271305+AMINEhclh@users.noreply.github.com>
2022-11-24 23:01:14 +0100

    Add files via upload

---
+++ submit.js
@@ -0,0 +1,27 @@
+function dis() {  
+    
+    document.getElementById("t1").disabled = true;
+    document.getElementById("t2").disabled = true;
+    document.getElementById("t3").disabled = true;
+    document.getElementById("t4").disabled = true;
+    document.getElementById("t5").disabled = true;
+    document.getElementById("t6").disabled = true;
+ }  
+     function dis2() {  
+    
+        document.getElementById("t1").disabled = false;
+        document.getElementById("t2").disabled = false;
+        document.getElementById("t3").disabled = false;
+        document.getElementById("t4").disabled = false;
+        document.getElementById("t5").disabled = false;
+        document.getElementById("t6").disabled = false;
+     }  
+
+
+
+
+
+    
+       
+    
+    
